73f272ced4bbc822eb85c092826dc90bc2f4f13d
charlielopez123 <128399755+charlielopez123@users.noreply.github.com>
2024-05-04 20:23:34 +0200

    added a via point for the box lifting

--- Codes/inv_kin_adj_hold_box.py
+++ Codes/inv_kin_adj_hold_box.py
@@ -27,11 +27,13 @@ q = sim.inverse_kinematics_adjusted(x_des, q_ref = q_ref)
 sim.animate(q)
 
 #sim.animate(q_ref)
+x_des = sim.x_des(target = key_points["via_point_lift_box"])
+q = sim.inverse_kinematics_adjusted(x_des, q_ref = q_ref)
+sim.animate(q)
 
 x_des = sim.x_des(target = key_points["box_up"])
 q = sim.inverse_kinematics_adjusted(x_des, q_ref = robot_poses["q_hold_box_up"])
 sim.animate(q)
-
 
 #box held up high correctly with right config
 #<key qpos='0.2447 4.12114 -0.60945 -0.2447 4.12114 -0.60945 2.94472e-05 -1.57079 3.14159 -2.94472e-05 -1.57079 3.14159'/>#
