5fe14d1470c829a883c0b12cb03cf8b4ef4378a5
charlielopez123 <128399755+charlielopez123@users.noreply.github.com>
2024-05-16 23:07:49 +0200

    streamlined the call of the different TOPPRA animations

--- Codes/TOPPRA_speed_IK.py
+++ Codes/TOPPRA_speed_IK.py
@@ -14,16 +14,6 @@ sim = SoloSim(q_init = q_init)
 
 sim.all_the_points()
 
-#get the arms up to the strating position
-x_des = sim.x_des(target = key_points["arms_up"])
-q = sim.inverse_kinematics_adjusted(x_des, q_ref = q_ref)
-sim.speed_animate(q, num_time_steps= 3, t_max = 3)
-
-#parameters
-points = [
-    key_points["arms_up"],
-    key_points["via_point1_get2box"],
-    key_points["via_point2_get2box"],
-    key_points["box_high"]
-    ]
-sim.TOPPRA_speed_animate(points, timed = True)
+for mvmt in key_movements:
+    points = key_movements[mvmt]
+    sim.TOPPRA_speed_animate(points, timed = True)
