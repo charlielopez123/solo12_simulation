d3a27fdf69e7b173437a1abbd0fd4e1809fab9dc
charlielopez123 <128399755+charlielopez123@users.noreply.github.com>
2024-05-17 14:12:04 +0200

    modifs

---
+++ Codes/Physics_TOPPRA_speed_IK.py
@@ -0,0 +1,19 @@
+# Testing of inverse_kinematics_adjusted() function for holding the box up position using 'q_arms_up' as a q_ref for the solver
+#Using the TOPPRA_speed_animate() function in Simulation.py to animate the movement using the generated velocity profiles from TOPPRA() in Simulation.py
+import mujoco
+import mujoco.viewer as viewer
+import numpy as np
+from solo import Robot
+from time import sleep
+import time
+from Simulation import SoloSim
+from Joint_positions import *
+
+q_init = robot_poses["q_init"]
+sim = SoloSim(q_init = q_init)
+
+sim.all_the_points()
+
+for mvmt in key_movements:
+    points = key_movements[mvmt]
+    sim.TOPPRA_speed_animate(points, timed = True)
