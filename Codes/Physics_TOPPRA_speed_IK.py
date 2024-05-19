9a1c3f804c17e660c87bc0fe61f56ca7677dfeb3
charlielopez123 <128399755+charlielopez123@users.noreply.github.com>
2024-05-19 10:59:46 +0200

    small modifs

--- Codes/Physics_TOPPRA_speed_IK.py
+++ Codes/Physics_TOPPRA_speed_IK.py
@@ -14,33 +14,11 @@ sim = SoloSim(q_init = q_init)
 
 sim.all_the_points()
 
-sim.model.opt.timestep
-
 sim.robot.data.ctrl = q_init
-
-#sim.workspace(15)
 
 points = key_movements['lift_arms']
 timed = False
 
 sim.simulate() #simulate forward in time to get started in the physics environment
 
-# N_samples = len(points)
-# duration, optimal_path = sim.TOPPRA(points)
-
-sim.TOPPRA_speed_animate(points, ctrl = False)
-
-# if timed:
-#     start_time = time.time()
-
-# q = sim.robot.get_q()
-# for i in range(len(optimal_path)):
-#     q = optimal_path[i]
-#     sim.robot.set_q(q)
-#     sim.robot.step()
-#     if sim.v is not None:
-#         sim.v.sync()
-
-# if timed:
-#     end_time = time.time()
-#     simulation_time = end_time - start_time
+sim.TOPPRA_speed_animate(points, ctrl = True)
