6cb8b530533b72b5b1b7fedffb2692179a6e9a2c
charlielopez123 <128399755+charlielopez123@users.noreply.github.com>
2024-05-31 23:25:55 +0200

    added documentation

--- Simulation.py
+++ Simulation.py
@@ -12,7 +12,7 @@ import toppra.constraint as constraint
 import toppra.algorithm as algo
 import numpy as np
 import matplotlib.pyplot as plt
-
+from Joint_positions import *
 class SoloSim:
   """
   A class for simulating a Solo robot in a MuJoCo environment.
@@ -20,13 +20,17 @@ class SoloSim:
 
   def __init__(self,
             q_init=None,
-            launch_viewer = True):
+            launch_viewer = True,
+            manipulation_task = True,
+            use_hind_legs = False):
     """
-    Initializes the simulation environment.
+    Initializes the simulation environment for the Solo robot.
 
     Args:
-      xml_path: Path to the MuJoCo scene XML file.
-      q_init: Initial joint positions for the robot (optional).
+        q_init (array, optional): Initial joint positions for the robot. Defaults to None.
+        launch_viewer (bool, optional): Flag to launch the MuJoCo viewer. Defaults to True.
+        manipulation_task (bool, optional): Flag to indicate if the simulation is for a manipulation task. If True, inverse kinematics for hind legs will be skipped. Defaults to True.
+        use_hind_legs (bool, optional): Flag to indicate whether to use hind legs for manipulation tasks. Defaults to False.
     """
     # Load scene and model
     self.model = mujoco.MjModel.from_xml_path('scene.xml')
@@ -40,26 +44,29 @@ class SoloSim:
     self.dt_control = self.model.opt.timestep
 
     #If in a context of a manipulation task, skip the IK computation of the hind legs
-    self.manipulation_task = True
+    self.manipulation_task = manipulation_task
+
+    #Choose which legs to use for manipulation task
+    self.use_hind_legs = use_hind_legs
 
     #Define the velocity limits (rad/s) and acceleration limits
-    self.vlim = 2
-    self.alim = 5
+    self.vlim = 0.5
+    self.alim = 3
 
     #Define the control rate with the time step
     self.dt = 0.001
 
   def animate(self, q_2, q_1 = None, t_max = 2, dt = 0.01, timed=False):
       """
-      Animates a transition between two robot joint configurations.
+    Animates a transition between two robot joint configurations.
 
-      Args:
-        q_1: Starting joint positions.
-        q_2: Ending joint positions.
-        t_max: Maximum simulation time.
-        dt: Time step for simulation.
-        timed: Whether to print the simulation time (optional).
-      """
+    Args:
+      q_2 (array): Ending joint positions.
+      q_1 (array, optional): Starting joint positions. Defaults to current positions if None.
+      t_max (float, optional): Maximum simulation time. Defaults to 2 seconds.
+      dt (float, optional): Time step for simulation. Defaults to 0.01 seconds.
+      timed (bool, optional): Flag to print the simulation time. Defaults to False.
+    """
       if q_1 is None:
         q_1 = self.robot.get_q()
       if timed:
@@ -76,7 +83,7 @@ class SoloSim:
         simulation_time = end_time - start_time
         print(f"Simulation time: {simulation_time:.2f} seconds")
 
-  def get_hand_positions(self):
+  def get_front_hand_positions(self):
       """
       Retrieves the positions of the robot's front "hands".
 
@@ -90,6 +97,22 @@ class SoloSim:
       # FR_FOOT Position
       x = self.robot.fk_pose(q=self.robot.get_q(), EE_name="FR_FOOT")
       positions["FR_FOOT"] = x
+      return positions
+
+  def get_hind_hand_positions(self):
+      """
+      Retrieves the positions of the robot's hind "hands".
+
+      Returns:
+        A dictionary containing the positions of the HL_FOOT and HR_FOOT.
+      """
+      positions = {}
+      # HL_FOOT Position
+      x = self.robot.fk_pose(q=self.robot.get_q(), EE_name="HL_FOOT")
+      positions["HL_FOOT"] = x
+      # HR_FOOT Position
+      x = self.robot.fk_pose(q=self.robot.get_q(), EE_name="HR_FOOT")
+      positions["HR_FOOT"] = x
       return positions
 
   def visualize_point(self, key_pos, rgba=np.array([0.8, 0.3, 0.3, 0.5]), verbose=False):
@@ -131,60 +154,63 @@ class SoloSim:
           print({self.v.user_scn.geoms[id]})
         self.v.sync()
 
-  def inverse_kinematics_adjusted(self, x_des, q_ref=None, q = None):
+  def inverse_kinematics_adjusted(self, x_des, q_ref=None, q=None):
+      """
+      Computes the inverse kinematics for a desired end-effector position.
+
+      Args:
+        x_des (dict): Desired positions of the end-effectors in Cartesian space (x, y, z).
+        q_ref (array, optional): Reference joint positions for the inverse kinematics. Defaults to current positions.
+        q (array, optional): Current joint positions. Defaults to current positions.
+
+      Returns:
+        array: Joint positions to achieve the desired end-effector positions.
+      """
+      if q_ref is None:
+        q_ref = self.robot.get_q()
+      if q is None:
+        q = self.robot.get_q()
+
+      noise = self.build_noise()
+
+      if not self.use_hind_legs:
+        EE_list = ['FL_FOOT', 'FR_FOOT']
+      else:
+        EE_list = ['HL_FOOT', 'HR_FOOT']
+      
+      if self.manipulation_task: #If a manipulation task, only compute IK for front hands
+        for EE in EE_list:
+          q_ik, success = self.robot.inverse_kinematics(x_des[EE], q_ref=q_ref, EE_name=EE, noise=noise)
+          print(f"For {EE}: {success}")
+          for i in EE_joints[EE]:
+            q[i] = q_ik[i]
+      else:
+        for EE in EE_joints:
+          q_ik, success = self.robot.inverse_kinematics(x_des[EE], q_ref=q_ref, EE_name=EE, noise=noise)
+          print(f"For {EE}: {success}")
+          for i in EE_joints[EE]:
+            q[i] = q_ik[i]
+
+      return q
+
+  def all_the_points(self, set_of_points = key_points):
     """
-    Inverse kinematics function calling the inverse_kinematics function from solo.py for a desired position of an end-effector
-    which is then adjusted by keeping all the other joints unaffecting the chosen EE's position to the current configuration
+    Visualizes all the points in the viewer.
 
     Args:
-      x_des: desired position of the end-effector [x, y, z]
-      q_ref: reference joint position from which the inverse kinematics are computed from, by default set as None
-      EE_name: End-effector name from ["FL_FOOT", "FR_FOOT", "HL_FOOT", "HR_FOOT"], by default "FR_FOOT",
-      q: current joint disposition before setting it to the robot
-
-    Returns:
-    q: solution returned by the solver for the joint positions to reach the EE to x_des position
-    success: True or False, whether the solver managed to converge towards a solution or not
+      set_of_points (dict): Dictionary containing the points to visualize and their properties.
     """
-
-    if q_ref is None:
-      q_ref = self.robot.get_q()
-
-    if q is None:
-      q = self.robot.get_q()
-
-    noise = self.build_noise()
-
-    if self.manipulation_task == True:
-      for EE in ['FL_FOOT', 'FR_FOOT']:
-        q_ik, success = self.robot.inverse_kinematics(x_des[EE], q_ref = q_ref, EE_name = EE, noise = noise)
-        print(f"for {EE}: {success}")
-        for i in EE_joints[EE]: # change the relevant joints of the EE
-          q[i] = q_ik[i]
-    else:
-      for EE in EE_joints:
-        q_ik, success = self.robot.inverse_kinematics(x_des[EE], q_ref = q_ref, EE_name = EE, noise = noise)
-        print(f"for {EE}: {success}")
-        for i in EE_joints[EE]: # change the relevant joints of the EE
-          q[i] = q_ik[i]
-
-    return q
-
-  def all_the_points(self):
-    """
-    Visualize all the points in the viewer
-    """
-    for situation in key_points:
-      rgba = key_points[situation]['rgba']
-      for EE in key_points[situation]['pos']:
-        self.visualize_point(key_points[situation]['pos'][EE], rgba)
+    for situation in set_of_points:
+      rgba = set_of_points[situation]['rgba']
+      for EE in set_of_points[situation]['pos']:
+        self.visualize_point(set_of_points[situation]['pos'][EE], rgba)
 
   def get_joint_positions(self, EE):
     """
-    Prints the positions of the chosen EE's corresponding joints with their names
+    Prints the positions of the chosen end-effector's corresponding joints.
 
     Args:
-      EE : Chosen EE from which we want information on the corresponding joints
+      EE (str): Chosen end-effector name.
     """
 
     q = self.robot.get_q()
@@ -192,11 +218,16 @@ class SoloSim:
       print(f"Joint {name_joints[joint]} Position: {q[joint]}")
 
   def build_noise(self):
-    # Build the noise array that we add to the x0 initial solution of th IK Solver
-    # We want a symmetric noise as to converge to a symmetric solution for both EE
+    """
+    Builds a noise array to add to the x0 initial solution of the IK solver.
+    We want a symmetric noise as to converge to a symmetric solution for both symmetric EE
+
+    Returns:
+      array: Noise array to ensure symmetric solutions for end-effectors.
+    """
 
     # Generate three random normal numbers for additional noise given to an EE
-    random_numbers = np.random.normal(size=3) # [a, b,c]
+    random_numbers = np.random.normal(size=3) # [a, b, c]
     random_numbers_negative = random_numbers * np.array([-1, 1, 1]) # [-a, b, c]
 
     pattern = np.concatenate((random_numbers, random_numbers_negative)) # [a, b, c, -a, b, c]
@@ -207,12 +238,12 @@ class SoloSim:
 
   def workspace(self, res = 10, rgba = np.array([0, 0, 1, 0.8]), EE_name = 'FL_FOOT'):
     """
-    Visualizes a cloud of possible EE positions as to get a rough estimate of the EE's workspace
+    Visualizes a cloud of possible end-effector positions to estimate the workspace.
 
     Args:
-      res: resolution of joint positional increments
-      rgba: rgb color and opacity of workspace points
-      EE_name : Chosen EE from which we want information on the corresponding workspace
+      res (int, optional): Resolution of joint positional increments. Defaults to 10.
+      rgba (array, optional): RGBA color and opacity of workspace points. Defaults to blue.
+      EE_name (str, optional): Chosen end-effector name. Defaults to 'FL_FOOT'.
     """
     # q_min = np.zeros(12)
     # q_max = 2*np.pi * np.ones(12)
@@ -235,6 +266,19 @@ class SoloSim:
                 self.visualize_point(x_FL[i, j, k], rgba=rgba)
 
   def velocity_profile(self, num_time_steps, full_duration, current, desired):
+    """
+    Deprecated, more optimal using TOPPRA
+    Generates an optimal velocity profile to minimize jerk for joint movements.
+
+    Args:
+      num_time_steps (int): Number of time steps for the velocity profile.
+      full_duration (float): Total duration of the movement.
+      current (array): Initial joint positions.
+      desired (array): Desired joint positions.
+
+    Returns:
+      array: Time-discretized joint positions following the optimal velocity profile.
+    """
     dt = full_duration/num_time_steps
 
     # Define your objective function to minimize jerk
@@ -285,15 +329,21 @@ class SoloSim:
     Returns:
     x_des: dictionary of the desired EEs positions
     """
-    x_des ={'FL_FOOT': target['pos']["left"],
-        'FR_FOOT': target['pos']["right"],
-        'HL_FOOT': self.robot.fk_pose(q=self.robot.get_q(), EE_name="HL_FOOT"), #keep current EE position for Hind Legs
-        'HR_FOOT': self.robot.fk_pose(q=self.robot.get_q(), EE_name="HR_FOOT")} #keep current EE position for Hind Legs
-    
+    if not self.use_hind_legs:
+      x_des ={'FL_FOOT': target['pos']["left"],
+          'FR_FOOT': target['pos']["right"],
+          'HL_FOOT': self.robot.fk_pose(q=self.robot.get_q(), EE_name="HL_FOOT"), #keep current EE position for Hind Legs
+          'HR_FOOT': self.robot.fk_pose(q=self.robot.get_q(), EE_name="HR_FOOT")} #keep current EE position for Hind Legs
+    else:
+      x_des ={'FL_FOOT': self.robot.fk_pose(q=self.robot.get_q(), EE_name="FL_FOOT"),
+          'FR_FOOT': self.robot.fk_pose(q=self.robot.get_q(), EE_name="FR_FOOT"),
+          'HL_FOOT': target['pos']["left"], #keep current EE position for Hind Legs
+          'HR_FOOT': target['pos']["right"]} #keep current EE position for Hind Legs
     return x_des
 
   def speed_animate(self, q_2, q_1 = None, t_max = 3, num_time_steps = 10, timed=False):
       """
+      Deprecated, more optimal to use TOPPRA trajectory profiles
       Animates a transition between two robot joint configurations with a velocity profile.
 
       Args:
@@ -329,14 +379,19 @@ class SoloSim:
 
   def TOPPRA(self, points):
     """
-    Defines the x_des dictionary to define all the desired positions of the front EEs whilst keeping the Hind legs put
+    Given an array of points in Cartesian space (x, y, z) that the EE has to follow: [start, ..., via_points, ..., end]. 
+    This function returns the optimal trajectories for the joints to follow the given points path with the starting point, 
+    end point and every via point in between for the movement.
 
     Args:
-        N_samples: Number of points in the path
-        points: array of the different via_points of the EE's path
+      points: array of the different via_points of the EE's path in in Cartesian space (x, y, z)
 
     Returns:
-    x_des: dictionary of the desired EEs positions
+      duration: duration of the given movement
+      ts_sample: time at each time step
+      qs_sample: array of all the different joint configurations at each time step  from start to finish
+      qds_sample: array of all the different joint speeds at each time step from start to finish
+      qdds_sample: array of all the different joint accelerations at each time step from start to finish
     """
 
     dof = 12
@@ -347,7 +402,7 @@ class SoloSim:
     alims = np.ones(dof)*self.alim #Acceleration constraints defined to be 5 rad/s^2
 
     way_pts.append(self.robot.get_q().tolist()) #Skip the IK computation of the starting as it is just the actual current position
-    for i in range(1, N_samples):
+    for i in range(1, N_samples): #Start IK computation from the first via_point
       x_des = self.x_des(target = points[i])
       q = self.inverse_kinematics_adjusted(x_des, q_ref = points[i]['q_ref']) # Use the appropriate q_ref for the IK computation
       way_pts.append(q.tolist())
@@ -368,24 +423,35 @@ class SoloSim:
     # The output trajectory is an instance of
     # :class:`toppra.interpolator.AbstractGeometricPath`.
     # ts_sample = np.linspace(0, jnt_traj.duration, 100)
+    duration = jnt_traj.duration
     ts_sample = np.arange(0, jnt_traj.duration, self.dt)
     qs_sample = jnt_traj(ts_sample) # ("Position (rad)")
     qds_sample = jnt_traj(ts_sample, 1) #("Velocity (rad/s)")
     qdds_sample = jnt_traj(ts_sample, 2) #("Acceleration (rad/s2)")
     ################################################################################
 
-    return jnt_traj.duration, qs_sample, qds_sample, qdds_sample
+    return duration, qs_sample, qds_sample, qdds_sample
 
   def TOPPRA_speed_animate(self, points, timed=False, ctrl = True, plot = False):
       """
-      Animates a transition between two robot joint configurations with a velocity profile.
+    Animates a transition between two robot joint configurations with a velocity profile
+    generated using the TOPPRA algorithm.
 
-      Args:
-        points: array of the different via_points for the given movement
-        timed: Boolean whether to time the movement animation using time.time()
-        ctrl: Boolean to choose whether to simulate the physics or not (using step() or forward())
-        plot: Boolean to gte the trajectories of the position, velocities and accelerations of the joints to later plot
-      """
+    Args:
+        points (list): Array of via_points for the movement in Cartesian space (x, y, z).
+        timed (bool, optional): Whether to time the movement animation using time.time(). Defaults to False.
+        ctrl (bool, optional): Whether to simulate the physics or not (using step() or forward()). Defaults to True.
+        plot (bool, optional): Whether to return the trajectories of positions, velocities, and accelerations
+                               of the joints for later plotting. Defaults to False.
+
+    Returns:
+        If plot is True, returns a tuple (duration, pos_sim, pos_TOPPRA, vel, acc):
+            duration (float): The duration of the movement.
+            pos_sim (np.ndarray): Array of joint positions at each time step during the simulation.
+            pos_TOPPRA (np.ndarray): Array of optimal joint positions at each time step generated by TOPPRA.
+            vel (np.ndarray): Array of joint velocities at each time step generated by TOPPRA.
+            acc (np.ndarray): Array of joint accelerations at each time step generated by TOPPRA.
+    """
 
       pos_sim = [] #initialize empty array to fill with q_joints at different time_steps using get_q()
       pos_TOPPRA = []
@@ -431,7 +497,12 @@ class SoloSim:
         return duration, np.array(pos_sim), np.squeeze(np.array(pos_TOPPRA)), np.squeeze(np.array(vel)), np.squeeze(np.array(acc))
 
   def simulate(self, steps=500):# 1000 steps => 4.3 - 5 seconds
-    # Simulate the environment for a little
+    """
+    Simulates the robot's environment for a specified number of steps.
+
+    Args:
+        steps (int, optional): The number of simulation steps to execute. Defaults to 500.
+    """
     for _ in range(steps):
         self.robot.step()
         self.v.sync()
