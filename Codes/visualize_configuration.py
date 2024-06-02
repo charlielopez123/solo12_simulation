a736ced0e19d4e4fcec4b792c7ab370e12ad7d38
charlielopez123 <128399755+charlielopez123@users.noreply.github.com>
2024-06-02 18:13:58 +0200

    added a configuration visualization script

---
+++ Codes/visualize_configuration.py
@@ -0,0 +1,20 @@
+#Visualize a certain configuration in 
+import mujoco
+import mujoco.viewer as viewer
+import numpy as np
+from solo import Robot
+from time import sleep
+import time
+from Simulation import SoloSim
+from Joint_positions import *
+
+q_init = robot_poses["q_init"]
+sim = SoloSim(q_init = q_init, use_hind_legs=False)
+
+#sim.all_the_points() # Visualize key points
+
+sleep(1)
+q = q_ref # Select a configuration from Joint_positions.py
+sim.robot.set_q(q, ctrl = False)
+sim.robot.forward()
+sim.v.sync()
