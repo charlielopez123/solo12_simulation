13ed22a515d1791988c0def272cb0d333eff8fbd
charlielopez123 <128399755+charlielopez123@users.noreply.github.com>
2024-06-09 16:04:40 +0200

    added doc

--- Codes/TOPPRA_full_task.py
+++ Codes/TOPPRA_full_task.py
@@ -27,18 +27,21 @@ sim.alim = 15
 #Show all the points
 sim.all_the_points()
 
+#Whether to simulate the physics or not
+ctrl = True # ctrl is True when simulating with physics, false if not
+
 #Initialize duration
 duration = 0
 # Iterate through key movements to calculate and animate optimal trajectories
 if not sim.use_hind_legs:
     for mvmt in key_movements:
         points = key_movements[mvmt]
-        dur = sim.TOPPRA_speed_animate(points, ctrl = True, plot = True)
+        dur = sim.TOPPRA_speed_animate(points, ctrl = ctrl, plot = True)
         duration += dur[0]
 else:
     for mvmt in key_movements_hind_legs:
         points = key_movements_hind_legs[mvmt]
-        dur = sim.TOPPRA_speed_animate(points, ctrl = True, plot = True)
+        dur = sim.TOPPRA_speed_animate(points, ctrl = ctrl, plot = True)
         duration += dur[0]
 
 print(duration)
